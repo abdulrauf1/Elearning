11e0e81144009c47ecbf57b0aa213cc6a84c7932
abdulrauf1 <47816353+abdulrauf1@users.noreply.github.com>
2022-06-19 08:41:36 -0700

    Add files via upload

---
+++ build/precache-manifest.c89c3aee036ffc0ce81f4c371cbeea8a.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc3f5670d5cba0e76b53",
+    "url": "/UniSV-fyp/static/css/main.8af9d3bf.chunk.css"
+  },
+  {
+    "revision": "cc3f5670d5cba0e76b53",
+    "url": "/UniSV-fyp/static/js/main.cc3f5670.chunk.js"
+  },
+  {
+    "revision": "75d03b13a3b36b8ce0e8",
+    "url": "/UniSV-fyp/static/js/1.75d03b13.chunk.js"
+  },
+  {
+    "revision": "4c3e11aabe62b54619a7",
+    "url": "/UniSV-fyp/static/js/runtime~main.4c3e11aa.js"
+  },
+  {
+    "revision": "16ca6711eda05dcc85aad3da4fdc0904",
+    "url": "/UniSV-fyp/static/media/script.16ca6711.mp4"
+  },
+  {
+    "revision": "916dc74688f61f4beca70188fccf99e2",
+    "url": "/UniSV-fyp/static/media/supporterLogo.916dc746.png"
+  },
+  {
+    "revision": "717e07a04254148f7b696f7754adecfd",
+    "url": "/UniSV-fyp/static/media/register-login.717e07a0.png"
+  },
+  {
+    "revision": "20b1a98c37aec75870cd66dc826279ad",
+    "url": "/UniSV-fyp/static/media/play_circle_filled_black_24dp.20b1a98c.svg"
+  },
+  {
+    "revision": "5cab5c52cbb55385ce080e90ae912e58",
+    "url": "/UniSV-fyp/static/media/menu.5cab5c52.svg"
+  },
+  {
+    "revision": "2ca824d367a602a825bda7e04d141854",
+    "url": "/UniSV-fyp/static/media/close.2ca824d3.svg"
+  },
+  {
+    "revision": "3af57ed3199bf831b8deaca886e7b1fb",
+    "url": "/UniSV-fyp/static/media/hero.3af57ed3.png"
+  },
+  {
+    "revision": "57500e47352ce3d6f71e7b032abecf13",
+    "url": "/UniSV-fyp/static/media/codeIcon.57500e47.svg"
+  },
+  {
+    "revision": "3761340ce485bf8e23fa7b6226d32c83",
+    "url": "/UniSV-fyp/static/media/video2.3761340c.mp4"
+  },
+  {
+    "revision": "d2c26579bda4bc2561804de5ec15878f",
+    "url": "/UniSV-fyp/static/media/video3.d2c26579.mp4"
+  },
+  {
+    "revision": "03bb87e7e5e2ed3f694e2626630c8b11",
+    "url": "/UniSV-fyp/static/media/search.03bb87e7.svg"
+  },
+  {
+    "revision": "6243a69d99336e26e01f8744a8353c75",
+    "url": "/UniSV-fyp/static/media/file.6243a69d.svg"
+  },
+  {
+    "revision": "b0ea40bd92bc48b762a8bbfdc6bdf2ae",
+    "url": "/UniSV-fyp/static/media/certification.b0ea40bd.svg"
+  },
+  {
+    "revision": "1b791f54bb45dfe702aeb7707e4a11cd",
+    "url": "/UniSV-fyp/static/media/email.1b791f54.svg"
+  },
+  {
+    "revision": "e4e52ca92337a8f9618a4381d45154ce",
+    "url": "/UniSV-fyp/static/media/call.e4e52ca9.svg"
+  },
+  {
+    "revision": "d7b84a3e5247a52e818acd13ff47ffb4",
+    "url": "/UniSV-fyp/static/media/instagram.d7b84a3e.svg"
+  },
+  {
+    "revision": "4c9e29b065015295b7df2c6573853e59",
+    "url": "/UniSV-fyp/static/media/facebook.4c9e29b0.svg"
+  },
+  {
+    "revision": "4ca920a1f11b8c0d2f9c174d8938782f",
+    "url": "/UniSV-fyp/static/media/linkedin.4ca920a1.svg"
+  },
+  {
+    "revision": "69188d76333fc056ec610c5261cef01e",
+    "url": "/UniSV-fyp/static/media/emailwhite.69188d76.svg"
+  },
+  {
+    "revision": "f32809824090342e6626287c7b98ad73",
+    "url": "/UniSV-fyp/static/media/callwhite.f3280982.svg"
+  },
+  {
+    "revision": "1f8373dee39fb9d2a368ee23b569b35e",
+    "url": "/UniSV-fyp/static/media/location.1f8373de.svg"
+  },
+  {
+    "revision": "9c5d12a44182bdc6e11f14e7a720469a",
+    "url": "/UniSV-fyp/static/media/error.9c5d12a4.png"
+  },
+  {
+    "revision": "8b600e077ec52df44b4ba11ac16f71b8",
+    "url": "/UniSV-fyp/static/media/courses.8b600e07.jpg"
+  },
+  {
+    "revision": "b30e038906fa07fe9fb5f4d65d7845f7",
+    "url": "/UniSV-fyp/static/media/aboutus.b30e0389.jpg"
+  },
+  {
+    "revision": "28999e4bc20470ca264ae56a4114d31c",
+    "url": "/UniSV-fyp/static/media/delete.28999e4b.svg"
+  },
+  {
+    "revision": "5d1fec6a742d12a5dc6f2f0c36886cbd",
+    "url": "/UniSV-fyp/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/UniSV-fyp/precache-manifest.c89c3aee036ffc0ce81f4c371cbeea8a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/UniSV-fyp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
